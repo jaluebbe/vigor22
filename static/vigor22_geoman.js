f544186bec60f31a6ce5d1eeb094114a3d6a367b
Jannis Lübbe <jannis@jl82.de>
2022-07-30 19:09:35 +0200

    added geoman controls to the map.

---
+++ static/vigor22_geoman.js
@@ -0,0 +1,9 @@
+map.pm.addControls({
+    position: 'topleft',
+    drawCircle: false,
+    drawPolyline: false,
+    drawRectangle: false,
+    drawCircleMarker: false,
+    drawMarker: false,
+    oneBlock: true,
+});
