137d1337dd924b7d7ae7eb6f1af5a0b0f519ba6c
Jannis Lübbe <jannis@jl82.de>
2022-08-02 23:46:02 +0200

    replacing panToBounds by fitBounds .

--- static/vigor22_geoman.js
+++ static/vigor22_geoman.js
@@ -35,7 +35,7 @@ function importShapes() {
         fr.onload = function(fileData) {
             var geojsonInput = JSON.parse(fileData.target.result);
             myImportedLayers.addData(geojsonInput);
-            map.panInsideBounds(myImportedLayers.getBounds());
+            map.fitBounds(myImportedLayers.getBounds());
         };
         fr.readAsText(fileInput.files[i])
     }
