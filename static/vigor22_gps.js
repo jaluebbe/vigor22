5aeed9805383d05ba1e4fe88b23698a7bf767a6b
Jannis Lübbe <jannis@jl82.de>
2022-08-13 17:52:10 +0200

    added draft for heading indictator and fertiliser range.

--- static/vigor22_gps.js
+++ static/vigor22_gps.js
@@ -22,6 +22,11 @@ var myCircle = L.circle([], {
     radius: 0,
     pmIgnore: true
 });
+var myPolyline = L.polyline([], {
+    color: 'red',
+    pmIgnore: true
+});
+
 
 function onLocationFound(e) {
     console.log(e);
@@ -31,11 +36,25 @@ function onLocationFound(e) {
     if (!map.hasLayer(myMarker)) {
         myMarker.addTo(map);
         myCircle.addTo(map);
+        myPolyline.addTo(map);
     }
     if (!map.getBounds().contains(e.latlng)) {
         map.setView(e.latlng);
     }
     info.updateContent(e.heading, e.speed);
+    var centerPoint = turf.point([e.longitude, e.latitude]);
+    if (e.heading === undefined) {
+        myPolyline.setLatLngs([]);
+    } else {
+        var frontPoint = turf.destination(centerPoint, 5e-3, e.heading);
+        var leftPoint = turf.destination(centerPoint, 15e-3, e.heading - 90);
+        var rightPoint = turf.destination(centerPoint, 15e-3, e.heading + 90);
+        myPolyline.setLatLngs([
+            [centerPoint.geometry.coordinates.reverse(), frontPoint.geometry.coordinates.reverse()],
+            [centerPoint.geometry.coordinates.reverse(), leftPoint.geometry.coordinates.reverse()],
+            [centerPoint.geometry.coordinates.reverse(), rightPoint.geometry.coordinates.reverse()]
+        ]);
+    }
 }
 
 map.setZoom(9)
