d1aedc96e48e8176926f75c3cd6d394fc9665cef
Jannis Lübbe <jannis@jl82.de>
2023-05-25 00:00:53 +0200

    upgraded vigor22 processing to latest version from GPSTracker .

--- static/gps_device.js
+++ static/gps_device.js
@@ -1,13 +1,27 @@
 map.on('locationfound', onLocationFound);
 map.on('locationerror', onLocationError);
-map.locate({
-    watch: true,
-    enableHighAccuracy: true
-});
-function resetGPSConnection() {
-    map.stopLocate();
+
+function connectGPS() {
     map.locate({
         watch: true,
         enableHighAccuracy: true
     });
+    noSleep.enable();
+    statusLightGPS.style["background-color"] = "#2dc937";
+    statusLabelGPS.innerHTML = "connected";
 };
+
+function disconnectGPS() {
+    map.stopLocate();
+    noSleep.disable();
+    running = false;
+    statusLightGPS.style["background-color"] = "#cc3232";
+    statusLabelGPS.innerHTML = "disconnected";
+};
+
+function resetGPSConnection() {
+    disconnectGPS();
+    connectGPS();
+    noSleep.enable();
+};
+
