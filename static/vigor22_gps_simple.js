948b9a6ad5a309d10f6952f0280c168ab69b349d
Jannis Lübbe <jannis@jl82.de>
2023-12-23 13:29:43 +0100

    Colourising the plan shape depending on the rate.

--- static/vigor22_gps_simple.js
+++ static/vigor22_gps_simple.js
@@ -50,13 +50,23 @@ var boundariesLayer = L.geoJSON([], {
         });
     }
 });
+function colourisePlan(rate) {
+    if (rate == 1)
+        return "#997a00";
+    else if (rate >= 0.8)
+        return "#ffcc00";
+    else if (rate >= 0.6)
+        return "#ffe066";
+    else
+        return "#fff5cc";
+}
 var planLayer = L.geoJSON([], {
     onEachFeature: onEachFeature,
     pane: 'plan',
     style: function(feature) {
         return styleShape(feature, {
-            fillColor: "#ffcc00",
-            fillOpacity: feature.properties.V22RATE / 3,
+            fillColor: colourisePlan(feature.properties.V22RATE),
+            fillOpacity: 0.3,
             weight: 1.5,
             color: "grey"
         });
